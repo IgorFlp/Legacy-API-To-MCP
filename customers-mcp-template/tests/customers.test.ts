c7612494ccdba7e165bc786b682c41a65c6cc6fa
IgorFlp <igor.felipe.13@hotmail.com>
2026-06-23 19:22:13 -0300

    Added client helper and list customer test

---
+++ customers-mcp-template/tests/customers.test.ts
@@ -0,0 +1,31 @@
+import { describe, it, after, before } from "node:test";
+import { createTestClient } from "./helpers.ts";
+import { Client } from "@modelcontextprotocol/sdk/client";
+import { type Customer } from "../src/domain/customer.ts";
+import assert  from "node:assert";
+
+type CustomersResult = {structuredContent: {customers:Customer[]}}
+
+describe("Customer MCP Suite",()=>{
+    let client: Client
+
+    before(async ()=>{
+       client = await createTestClient()
+    })
+    after(async ()=>{
+        await client.close()
+    })
+
+    it("Should list all cutomers", async()=>{
+        const result = await client.callTool({
+            name:"list_customers",
+            arguments:{}
+        }) as unknown as CustomersResult
+
+        assert.ok(
+            Array.isArray(result.structuredContent.customers),
+            "Should return an array of customers"
+        )
+
+    })
+})
