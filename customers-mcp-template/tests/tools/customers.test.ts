7a78e2b095d1540c2f7175e3a69f4b258c23c4b7
IgorFlp <igor.felipe.13@hotmail.com>
2026-06-24 22:51:41 -0300

    Added create customer test

--- customers-mcp-template/tests/tools/customers.test.ts
+++ customers-mcp-template/tests/tools/customers.test.ts
@@ -1,10 +1,11 @@
 import { describe, it, after, before } from "node:test";
 import { createTestClient } from "../helpers.ts";
 import { Client } from "@modelcontextprotocol/sdk/client";
-import { type Customer } from "../../src/domain/customer.ts";
+import { type Customer, type CreatedCustomer } from "../../src/domain/customer.ts";
 import assert  from "node:assert";
 
 type CustomersResult = {structuredContent: {customers:Customer[]}}
+type CreateCustomerResult = {structuredContent: CreatedCustomer}
 
 describe("Customer MCP Suite",()=>{
     let client: Client
@@ -16,7 +17,7 @@ describe("Customer MCP Suite",()=>{
         await client.close()
     })
 
-    it("Should list all cutomers", async()=>{
+    it("Should list all customers", async()=>{
         const result = await client.callTool({
             name:"list_customers",
             arguments:{}
@@ -28,4 +29,26 @@ describe("Customer MCP Suite",()=>{
         )
 
     })
+    it("Should create a new customer",async ()=>{
+        const customer = {
+                name: "Teste",
+                phone: "121-330",                
+            };
+        const result = await client.callTool({
+            name:"create_customer",
+            arguments: customer
+            
+        })as unknown as CreateCustomerResult
+        assert.ok(
+            result.structuredContent.id,
+            'Should contain customer ID'
+        )
+        assert.deepStrictEqual(
+            result.structuredContent.message,
+            `user ${customer.name} created!`,
+            'Should contain customer create message'
+        )
+    })
+
+
 })
